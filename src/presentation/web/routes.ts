9b793905b4faca968a9308d0b41234324e2d6477
Annas Rusdi Awandari <annas.wanda@gmail.com>
2024-01-09 20:25:56 +0700

    add middleware & upgrade to next14

--- src/presentation/web/routes.ts
+++ src/presentation/web/routes.ts
@@ -7,6 +7,8 @@ import { ErrorCode } from "@/common/utils";
 import { MentorController } from "./controllers/mentor-controller";
 import { BookController } from "./controllers/book-controller";
 import { BannerController } from "./controllers/banner-controller";
+import { asyncWrapper } from "./libs";
+import { AuthMiddleware } from "./middlewares/auth-middleware";
 
 @injectable()
 export class Routes extends Router {
@@ -15,14 +17,21 @@ export class Routes extends Router {
     private userController: UserController,
     private mentorController: MentorController,
     private bookController: BookController,
-    private bannerController: BannerController
+    private bannerController: BannerController,
+    private authMiddleware: AuthMiddleware
   ) {
     super();
     this.getRouter().get("/health-check", async (req, res) => res.send("SERVER IS UP"));
     this.getRouter().use(this.authController.getRouter());
-    this.getRouter().use(this.userController.getRouter());
+    this.getRouter().use(
+      asyncWrapper(this.authMiddleware.authenticated.bind(this.authMiddleware)),
+      this.userController.getRouter()
+    );
     this.getRouter().use(this.mentorController.getRouter());
-    this.getRouter().use(this.bookController.getRouter());
+    this.getRouter().use(
+      asyncWrapper(this.authMiddleware.authenticated.bind(this.authMiddleware)),
+      this.bookController.getRouter()
+    );
     this.getRouter().use(this.bannerController.getRouter());
     this.getRouter().all("*", async (req, res, next) => {
       next(new AppError(ErrorCode.NOT_FOUND, "Not found"));
